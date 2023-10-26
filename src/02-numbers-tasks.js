91a71ced2b1611cd4810a0e9ee89a85e0bc2e19f
Ria River <riria.river@gmail.com>
2023-10-26 12:02:04 +0300

    feat: implement getAngleBetweenVectors function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -112,8 +112,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1 * x2 + y1 * y2;
+  const getMagnitude = (x, y) => Math.sqrt(x ** 2 + y ** 2);
+
+  return Math.acos(dotProduct / (getMagnitude(x1, y1) * getMagnitude(x2, y2)));
 }
 
 /**
