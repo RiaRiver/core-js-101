fa066b0523367d094bbf978272a369b6611563ba
Ria River <riria.river@gmail.com>
2023-10-27 05:42:06 +0300

    feat: implement getIdentityMatrix function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -465,8 +465,10 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n },
+    (_, ind) => Array.from({ length: n },
+      (__, Sind) => (Sind === ind ? 1 : 0)));
 }
 
 /**
