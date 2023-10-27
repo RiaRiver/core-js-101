c45e11a4db79204934d3fa12d8290b76ca71532d
Ria River <riria.river@gmail.com>
2023-10-27 16:07:10 +0300

    feat: implement isInsideCircle function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -169,8 +169,12 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const xDistance = point.x - circle.center.x;
+  const yDistance = point.y - circle.center.y;
+  const pointDistance = Math.sqrt(xDistance ** 2 + yDistance ** 2);
+
+  return pointDistance < circle.radius;
 }
 
 
