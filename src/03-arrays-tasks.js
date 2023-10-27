f0acf3893426327670d4b11928e618f99940d314
Ria River <riria.river@gmail.com>
2023-10-27 08:14:58 +0300

    feat: implement distinct function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -500,8 +500,9 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  // return Array.from(new Set(arr));
+  return arr.filter((value, ind, array) => !array.slice(0, ind).includes(value));
 }
 
 /**
