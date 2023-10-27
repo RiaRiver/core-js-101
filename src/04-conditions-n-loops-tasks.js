6cd25824cd8e08c7d6542a2351ab1538b8867162
Ria River <riria.river@gmail.com>
2023-10-27 16:22:56 +0300

    feat: implement findFirstSingleChar function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -189,8 +189,13 @@ function isInsideCircle(circle, point) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    if (str.lastIndexOf(char) === str.indexOf(char)) return char;
+  }
+
+  return null;
 }
 
 
