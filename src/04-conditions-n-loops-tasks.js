d5644caa5fd20f05114d085ad56e4220c20ccdac
Ria River <riria.river@gmail.com>
2023-10-27 12:53:48 +0300

    feat: implement isTriangle function

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -92,8 +92,11 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  const sidesSum = a + b + c;
+  const maxSide = Math.max(a, b, c);
+
+  return (sidesSum - maxSide) > maxSide;
 }
 
 
